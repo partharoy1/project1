1ba149898feae13764b9b01bb887ec1008ef7c27
Partha Roy <roy.partha444@gmail.com>
2017-12-08 17:08:55 -0500

    This is my first commit

---
+++ bin/classNobject/TestHouse.java class 2
@@ -0,0 +1,11 @@
+package classNobject;
+
+public class TestHouse {
+
+	public static void main(String[] args) {
+		House myHouse = new House ();
+		myHouse.showHouseName();
+
+	}
+
+}
---
+++ src/classNobject/House.java
@@ -0,0 +1,13 @@
+package classNobject;
+
+public class House {
+		
+		int houseNumber;
+		String houseName = "whiteHouse";
+		public void showHouseName() {
+			System.out.println(houseName);
+		
+		}
+	
+
+}
